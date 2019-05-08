d02275f7b1a3128d8f717d92c34b563ff4b110ef
Magnus Ottenklinger <evnu@posteo.de>
2019-05-08 22:57:02 +0200

    Reformat with brittany

--- xmonad.hs
+++ xmonad.hs
@@ -1,25 +1,21 @@
-import XMonad
-import XMonad.Actions.CycleWS
-import XMonad.Util.EZConfig
-import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.DynamicLog
+import           XMonad
+import           XMonad.Actions.CycleWS
+import           XMonad.Util.EZConfig
+import           XMonad.Hooks.ManageDocks
+import           XMonad.Hooks.DynamicLog
 
 meta = mod4Mask
 
 main = xmonad =<< xmobar myConfig
 
-myConfig = def
-    { modMask = meta
-    , terminal = "urxvt"
-    , layoutHook = avoidStruts $ layouts
-    }
+myConfig =
+  def { modMask = meta, terminal = "urxvt", layoutHook = avoidStruts $ layouts }
     `additionalKeys` keyBindings
 
-layouts = Mirror $ Tall 1 (3/100) (1/2) ||| Full
+layouts = Mirror $ Tall 1 (3 / 100) (1 / 2) ||| Full
 
 keyBindings =
-    [
-         ((meta, xK_f), toggleWS)
-       , ((meta, xK_Left),  prevWS)
-       , ((meta, xK_Right), nextWS)
-    ]
+  [ ((meta, xK_f)    , toggleWS)
+  , ((meta, xK_Left) , prevWS)
+  , ((meta, xK_Right), nextWS)
+  ]
