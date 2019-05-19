ea500c178a8a4098da4f53ca2044f9a888bf20e9
Magnus Ottenklinger <evnu@posteo.de>
2019-05-19 11:10:30 +0200

    Larger border and change focused color

--- xmonad.hs
+++ xmonad.hs
@@ -9,10 +9,16 @@ meta = mod4Mask
 main = xmonad =<< xmobar myConfig
 
 myConfig =
-  def { modMask = meta, terminal = "urxvt", layoutHook = avoidStruts $ layouts }
+  def { modMask            = meta
+      , terminal           = "urxvt"
+      , layoutHook         = avoidStruts $ layouts
+      , borderWidth        = 6
+      , focusedBorderColor = "#42adf4"
+      }
     `additionalKeys` keyBindings
 
-layouts = (Mirror $ Tall 1 (3 / 100) (1 / 2)) ||| Tall 1 (3 / 100) (1 / 2) ||| Full
+layouts =
+  (Mirror $ Tall 1 (3 / 100) (1 / 2)) ||| Tall 1 (3 / 100) (1 / 2) ||| Full
 
 keyBindings =
   [ ((meta, xK_f)    , toggleWS)
